7deec5c55c20f116266f6b61907cd84f106c8c52
BOTOOM <edwardiaz.dev@gmail.com>
2023-10-05 14:30:07 -0500

    add books to user list

--- Lab-5 - DynamoDb y S3/DynamoDB/src/handlers/get-all-items.mjs
+++ Lab-5 - DynamoDb y S3/DynamoDB/src/handlers/get-all-items.mjs
@@ -1,44 +1,66 @@
 // Create clients and set shared const values outside of the handler.
 
 // Create a DocumentClient that represents the query to add an item
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
 // Get the DynamoDB table name from environment variables
 const tableName = process.env.USERS_TABLE;
+const bookTableName = process.env.BOOK_TABLE;
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
 export const getAllItemsHandler = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
-    }
-    // All log statements are written to CloudWatch
-    console.info('received:', event);
+  if (event.httpMethod !== "GET") {
+    throw new Error(
+      `getAllItems only accept GET method, you tried: ${event.httpMethod}`
+    );
+  }
+  // All log statements are written to CloudWatch
+  console.info("received:", event);
 
-    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-    var params = {
-        TableName : tableName
+  // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+  // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+  var params = {
+    TableName: tableName,
+  };
+
+  try {
+    const data = await ddbDocClient.send(new ScanCommand(params));
+    var items = data.Items;
+  } catch (err) {
+    console.log("Error", err);
+  }
+  itams.map(async (item) => {
+    var bookparams = {
+      TableName: bookTableName,
+      Key: { user_id: item.id },
     };
-
     try {
-        const data = await ddbDocClient.send(new ScanCommand(params));
-        var items = data.Items;
+        const data = await ddbDocClient.send(new ScanCommand(bookparams));
+        var Bookitems = data.Items;
     } catch (err) {
-        console.log("Error", err);
+      console.log("Error", err);
     }
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(items)
+    return {
+      ...item,
+      books: Bookitems
     };
+  });
 
-    // All log statements are written to CloudWatch
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
-}
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
+
+  // All log statements are written to CloudWatch
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
+};
