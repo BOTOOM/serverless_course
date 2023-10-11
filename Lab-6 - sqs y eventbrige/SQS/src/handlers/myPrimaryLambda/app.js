79be6f7f7e6196fde33b62c0aaacbfb434229ead
BOTOOM <edwardiaz.dev@gmail.com>
2023-10-11 17:09:57 -0500

    add dead lambda

--- Lab-6 - sqs y eventbrige/SQS/src/handlers/myPrimaryLambda/app.js
+++ Lab-6 - sqs y eventbrige/SQS/src/handlers/myPrimaryLambda/app.js
@@ -3,18 +3,25 @@ import { v4 as uuidv4 } from "uuid";
 const sqs = new AWS.SQS();
 
 export const lambda_handler = async (event) => {
-
   // const groupId = event["groupId"]+ `${Date.now()}`;
+  const messages = [];
+  for (i = 0; i <= 10; i++) {
+    messages.push({
+      MessageBody: JSON.stringify({
+        "id": `mi-id-${i}`,
+        "message": `hola ${i}`,
+        "verificador": i
+      }),
+    });
+  }
   try {
-    if (SQSEntries.length > 0) {
-      let queueUrl;
-      
-      const sqs_status = await sqs
-        .sendMessageBatch({
-          QueueUrl: queueUrl,
-          Entries: SQSEntries,
-        })
-        .promise();
+    if (messages.length > 0) {
+      const queueUrl = process.env.MY_SQS_URL;
+
+      const sqs_status = await sqs.sendMessageBatch({
+        QueueUrl: queueUrl,
+        Entries: messages,
+      });
 
       console.log(`Messages sent successfully: ${JSON.stringify(sqs_status)}`);
     }
@@ -22,6 +29,5 @@ export const lambda_handler = async (event) => {
     console.error(`Error sending messages: ${sqs_error}`);
   }
 
-
-  return { status: 200, msg: "Cronjob successfully processed" };
+  return { status: 200, msg: "los mensjes se han enviado exitosamente" };
 };
